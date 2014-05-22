a7e5886eeaee0907511899b79798b4b35d4e3927
Ryan Ruan <ryan.ruan@ericsson.com>
2014-05-22 17:15:58 +0800

    add hao-only-one-window function
    Change-Id: I7064998dfc311b2eed50e8f60645ba1ee89c251d

--- c-dev.el
+++ c-dev.el
@@ -1,8 +1,20 @@
-;; (add-hook 'c-mode-common-hook
-;; 	  '(lambda ()
-;; 	     (c-set-style "linux")
-;; 	     (setq indent-tabs-mode nil)
-;; 	     (setq c-basic-offset 4)
-;;              ;; cscope setup
-;;              (load-file "~/.emacs.d/cscope/contrib/xcscope/xcscope.el")
-;; ))
+(defun hao-only-one-window ()
+  "Leave only current window and the window which contains buffer *cscope*"
+  (interactive)  ;; No need to save excursion
+  (let ((origin-window (selected-window)))
+    (mapc (lambda (window)
+            (or (equal (buffer-name (window-buffer window)) "*cscope*")
+                (eq origin-window window)
+                (delete-window window)))
+          (window-list))))
+
+(add-hook 'c-mode-common-hook
+	  '(lambda ()
+	     (c-set-style "linux")
+	     (setq indent-tabs-mode nil)
+	     (setq c-basic-offset 4)
+             ;; cscope setup
+             (load-file "~/.emacs.d/cscope/contrib/xcscope/xcscope.el")
+             (global-set-key (kbd "M-.") 'cscope-find-global-definition-no-prompting)
+             (global-set-key (kbd "M-,") 'cscope-pop-mark)
+             (global-set-key (kbd "\C-x 1") 'hao-only-one-window)))
