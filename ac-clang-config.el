dfcbfbac6ef01e7dd43955cdd0bc099318dbf787
Ryan Ruan <ryan.ruan@ericsson.com>
2014-08-28 10:43:48 +0800

    Amelie theme
    Change-Id: Id8d28461d045c6d265527ba6aedd309b086aa911

--- ac-clang-config.el
+++ ac-clang-config.el
@@ -2,7 +2,7 @@
 ;; (setq ac-auto-start t)
 (setq ac-auto-start nil)
 (setq ac-quick-help-delay 0.5)
-(define-key ac-mode-map (kbd "\C-m") 'ac-complete-clang)
+(define-key ac-mode-map (kbd "M-RET") 'ac-complete-clang)
 ;; To find the include files in you system, please do:
 ;; $ echo "" | g++ -v -x c++ -E -
 (defun my-ac-config ()
