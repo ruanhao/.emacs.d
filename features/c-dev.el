b229ffe49501b2db145c7aced2c4a08374b5f9cc
Ryan Ruan <ryan.ruan@ericsson.com>
2015-02-28 12:02:09 +0800

    recover cscope
    Change-Id: Ib4c1f8e01f0423fb88a4c8d7198009f7a70dd7cd

--- features/c-dev.el
+++ features/c-dev.el
@@ -1,13 +1,3 @@
-(defun hao-only-one-cscope ()
-  "Leave only current window and the window which contains buffer *cscope*"
-  (interactive)  ;; No need to save excursion
-  (let ((origin-window (selected-window)))
-    (mapc (lambda (window)
-            (or (equal (buffer-name (window-buffer window)) "*cscope*")
-                (eq origin-window window)
-                (delete-window window)))
-          (window-list))))
-
 (add-hook 'c-mode-common-hook
 	  '(lambda ()
 	     (c-set-style "linux")
@@ -15,15 +5,14 @@
 	     (setq indent-tabs-mode nil)
 	     (setq c-basic-offset 4)
              (modify-syntax-entry ?_ "w")
-             ;; cscope setup
-             ;; (load-file "~/.emacs.d/cscope/contrib/xcscope/xcscope.el")
-             ;; (setq cscope-do-not-update-database t)
-             ;; (define-key c-mode-map (kbd "M-.") 'cscope-find-global-definition-no-prompting)
-             ;; (define-key c-mode-map (kbd "M-,") 'cscope-pop-mark)
 
-             (define-key c-mode-map (kbd "M-RET") 'ac-complete-clang)
-             ;; (global-set-key (kbd "\C-x 1") 'hao-only-one-cscope)
-             ))
+             ;; cscope setup
+             (load-file "~/.emacs.d/cscope/contrib/xcscope/xcscope.el")
+             (setq cscope-do-not-update-database t)
+             (define-key c-mode-map (kbd "M-.") 'cscope-find-global-definition-no-prompting)
+             (define-key c-mode-map (kbd "M-,") 'cscope-pop-mark)
+
+             (define-key c-mode-map (kbd "M-RET") 'ac-complete-clang)))
 
 (defun my:ac-c-headers-init ()
   (require 'auto-complete-c-headers)
