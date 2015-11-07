9223a9e8f5eecd042e59b14b665552f6a2a454a6
Hao Ruan <haoru@cisco.com>
2015-11-08 00:03:09 +0800

    add project-root feature

--- features/basic.el
+++ features/basic.el
@@ -224,3 +224,53 @@
 
 (setq mac-option-modifier nil)
 (setq mac-command-modifier 'meta)
+
+;; nxml-mode-hook
+(add-hook 'nxml-mode-hook
+	  (lambda ()
+            (define-key nxml-mode-map (kbd "M-RET") 'completion-at-point)))
+
+;; Project Root
+(load-file "~/.emacs.d/lisp/project-root.el")
+
+(setq project-roots
+      `(("Git Project"
+         :root-contains-files (".gitignore"))))
+
+(defun hao-ido-project-files ()
+  "Use ido to select a file from the project."
+  (interactive)
+  (let (my-project-root project-files tbl)
+    (unless project-details (project-root-fetch))
+    (setq my-project-root (cdr project-details))
+    ;; get project files
+    (setq project-files
+          (split-string
+           (shell-command-to-string
+            (concat "find "
+                    my-project-root
+                    " \\( -name \"*.svn\" -o -name \"*.git\" \\) -prune -o -type f -print | grep -E \"\.(java|xml|yang)$\"" ;; ADD FILENAME FILTER HERE
+                    )) "\n"))
+    ;; populate hash table (display repr => path)
+    (setq tbl (make-hash-table :test 'equal))
+    (let (ido-list)
+      (mapc (lambda (path)
+              ;; format path for display in ido list
+              (setq key (replace-regexp-in-string "\\(.*?\\)\\([^/]+?\\)$" "\\2|\\1" path))
+              ;; strip project root
+              (setq key (replace-regexp-in-string my-project-root "" key))
+              ;; remove trailing | or /
+              (setq key (replace-regexp-in-string "\\(|\\|/\\)$" "" key))
+              (puthash key path tbl)
+              (push key ido-list)
+              )
+            project-files
+            )
+      (find-file (gethash (ido-completing-read "project-files: " ido-list) tbl)))))
+(global-set-key (kbd "C-x f") 'hao-ido-project-files)
+
+;; Auto Revert
+(global-auto-revert-mode t)
+
+;; yang-mode
+(load-file "~/.emacs.d/lisp/yang-mode.el")
