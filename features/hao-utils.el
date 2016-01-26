934cd7b9ee7001c33c43ffe1bbbee75d16d5e6ef
Hao Ruan <haoru@cisco.com>
2016-01-26 12:12:05 +0800

    add some text util

--- features/hao-utils.el
+++ features/hao-utils.el
@@ -216,3 +216,17 @@ this function would move cursor to the beginning of the word"
   (yank))
 
 (global-set-key (kbd "M-i") 'hao-duplicate-line)
+
+(defun hao-sort-lines-by-length (reverse beg end)
+  "Sort lines by length."
+  (interactive "P\nr")
+  (save-excursion
+    (save-restriction
+      (narrow-to-region beg end)
+      (goto-char (point-min))
+      (let ;; To make `end-of-line' and etc. to ignore fields.
+          ((inhibit-field-text-motion t))
+        (sort-subr reverse 'forward-line 'end-of-line nil nil
+                   (lambda (l1 l2)
+                     (apply #'< (mapcar (lambda (range) (- (cdr range) (car range)))
+                                        (list l1 l2)))))))))
