6f65aea001501e9d23eecbd30388d087ade1938c
Hao Ruan <haoru@cisco.com>
2017-02-07 15:58:05 +0800

    update python snippet

--- features/python-dev.el
+++ features/python-dev.el
@@ -8,4 +8,5 @@
             (define-key elpy-mode-map (kbd "C-x M-.") 'elpy-goto-definition-other-window)
             (define-key elpy-mode-map (kbd "M-,") 'pop-tag-mark)
             (define-key elpy-mode-map (kbd "M-RET") 'elpy-company-backend)
+            (highlight-indentation-mode -1)
             ))
