be2d8fca2675091c9eb5ba9ddba79d7fd8bc7200
Hao Ruan <haoru@cisco.com>
2017-07-04 12:41:25 +0800

    update auto-complete

--- features/ac-config.el
+++ features/ac-config.el
@@ -1,7 +1,8 @@
+
 ;; auto-complete
-(add-to-list 'load-path "~/.emacs.d/auto-complete")
+;; (add-to-list 'load-path "~/.emacs.d/auto-complete")
 (require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/ac-dict")
+;; (add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/ac-dict")
 (ac-config-default)
 (global-auto-complete-mode t)
 (ac-linum-workaround) ;; fix auto-complete-mode and linum-mode annoyance
