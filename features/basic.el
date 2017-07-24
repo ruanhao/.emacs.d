1f3815d5e95f3c228c6a12f8851d35eba2c83e3a
Hao Ruan <ruanhao1116@gmail.com>
2017-07-24 23:15:09 +0800

    update

--- features/basic.el
+++ features/basic.el
@@ -14,7 +14,7 @@
 ;; use M-x package-refresh-contents to reload the list of packages for the first time
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'package)
-(setq  package-archives 
+(setq  package-archives
        '(("gnu" . "http://elpa.gnu.org/packages/")
 	 ("org" . "http://orgmode.org/elpa/")
 	 ("melpa" . "http://melpa.org/packages/")
