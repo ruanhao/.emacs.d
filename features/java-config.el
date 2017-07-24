12bc4f14ec53e672707679dc211e12d6d2c1e4a2
Hao Ruan <haoru@cisco.com>
2017-07-24 15:18:42 +0800

    update java-config for tooltip color

--- features/java-config.el
+++ features/java-config.el
@@ -44,3 +44,24 @@
                                    'hi-red-b)
   )
 (add-hook 'java-mode-hook 'hilite-todos)
+
+;; tooltip colors
+(custom-set-faces
+ ;; ...
+ '(company-preview ((t (:background "black" :foreground "red"))))
+ '(company-preview-common ((t (:foreground "red"))))
+ '(company-preview-search ((t (:inherit company-preview))))
+ '(company-scrollbar-bg ((t (:background "brightwhite"))))
+ '(company-scrollbar-fg ((t (:background "red"))))
+ '(company-template-field ((t (:background "magenta" :foreground "black"))))
+ '(company-tooltip ((t (:background "brightwhite" :foreground "black"))))
+ '(company-tooltip-annotation ((t (:background "brightwhite" :foreground "black"))))
+ '(company-tooltip-annotation-selection ((t (:background "color-253"))))
+ '(company-tooltip-common ((t (:background "brightwhite" :foreground "red"))))
+ '(company-tooltip-common-selection ((t (:background "color-253" :foreground "red"))))
+ '(company-tooltip-mouse ((t (:foreground "black"))))
+ '(company-tooltip-search ((t (:background "brightwhite" :foreground "black"))))
+ '(company-tooltip-selection ((t (:background "color-253" :foreground
+ "black"))))
+ ;; ...
+)
