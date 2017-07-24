16d19fe1c3ca26c35ea69c7c296f03e200e6c647
Hao Ruan <ruanhao1116@gmail.com>
2017-07-24 22:54:17 +0800

    update script

--- features/basic.el
+++ features/basic.el
@@ -13,17 +13,20 @@
 ;; package setup
 ;; use M-x package-refresh-contents to reload the list of packages for the first time
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(package-initialize)
 (require 'package)
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("org" . "http://orgmode.org/elpa/")
-                         ("melpa" . "http://melpa.org/packages/")
-                         ("melpa-stable" . "http://stable.melpa.org/packages/"))
-      package-archive-priorities '(("melpa-stable" . 1)))
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
+(setq  package-archives 
+       '(("gnu" . "http://elpa.gnu.org/packages/")
+	 ("org" . "http://orgmode.org/elpa/")
+	 ("melpa" . "http://melpa.org/packages/")
+	 ("melpa-stable" . "http://stable.melpa.org/packages/"))
+       package-archive-priorities '(("melpa-stable" . 1)))
+(package-initialize)
 (setq use-package-verbose t)
 (setq use-package-always-ensure t)
+(if (not (package-installed-p 'use-package))
+    (progn
+      (package-refresh-contents)
+      (package-install 'use-package)))
 (require 'use-package)
 (use-package auto-compile
   :config (auto-compile-on-load-mode))
--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 DIR=$( dirname $0 )
 CONFIG_LIST=$( grep -v '^[[:space:]]*#' $DIR/emacs.config )
-> $DIR/init.el
+echo ";; -*- coding: utf-8 -*-" > $DIR/init.el
+echo ";; author: Hao Ruan" >> $DIR/init.el
+echo ";; date  : `date`" >> $DIR/init.el
+echo >> $DIR/init.el
 
 for item in $CONFIG_LIST; do
     echo "Generating config ($item)..."
