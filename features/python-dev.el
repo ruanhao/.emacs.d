3e5368f63096e52f197f8ffdf08d4db73662cbf1
Hao Ruan <haoru@cisco.com>
2017-07-25 14:33:22 +0800

    load elpy when in python mode

--- features/python-dev.el
+++ features/python-dev.el
@@ -3,13 +3,13 @@
 ;; Python IDE
 ;;;;;;;;;;;;;;;;;;;;
 (use-package elpy
-  :demand)
-(elpy-enable)
+  :defer t
+  :init (with-eval-after-load 'python (elpy-enable)))
+
 (add-hook 'elpy-mode-hook
           (lambda ()
-            (define-key elpy-mode-map (kbd "M-.") 'elpy-goto-definition)
-            (define-key elpy-mode-map (kbd "C-x M-.") 'elpy-goto-definition-other-window)
-            (define-key elpy-mode-map (kbd "M-,") 'pop-tag-mark)
-            (define-key elpy-mode-map (kbd "M-RET") 'elpy-company-backend)
             (highlight-indentation-mode -1)
-            ))
+            (define-key elpy-mode-map (kbd "M-.")     'elpy-goto-definition)
+            (define-key elpy-mode-map (kbd "C-x M-.") 'elpy-goto-definition-other-window)
+            (define-key elpy-mode-map (kbd "M-,")     'pop-tag-mark)
+            (define-key elpy-mode-map (kbd "M-RET")   'elpy-company-backend)))
